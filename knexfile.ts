22587bee0660a949037175da1d2b37814045e2f4
Vasileios Markopoulos <vmarkop@gunet.gr>
2026-07-10 17:15:25 +0300

    Fix migration files path on built images

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,13 @@
 import type { Knex } from "knex";
+import path from "path";
 import { config } from "./config";
 
 const knexConfig: Knex.Config = {
     client: "mysql2",
     connection: config.db_config.connection,
     migrations: {
-        extension: "ts",
-        directory: "./src/db/migrations",
+        extension: path.extname(__filename).slice(1) || "js",
+        directory: path.join(__dirname, "src", "db", "migrations"),
     },
 };
 
